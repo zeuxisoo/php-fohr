0311071625be37c3c1fb6c5a2de0802a224095b0
Zeuxis Lo <seekstudio@gmail.com>
2014-05-12 17:58:45 +0800

    improve PHPMig config integrated with Hall\App

--- phpmig.php
+++ phpmig.php
@@ -1,28 +1,28 @@
 <?php
 date_default_timezone_set('Asia/Hong_Kong');
 
-use \Phpmig\Adapter;
-use \Pimple;
-
 define('WWW_ROOT',    dirname(__FILE__));
+define('APP_ROOT',    WWW_ROOT.'/hall');
 define('CONFIG_ROOT', WWW_ROOT.'/config');
+define('VENDOR_ROOT', WWW_ROOT.'/vendor');
+
+require VENDOR_ROOT.'/autoload.php';
+require APP_ROOT.'/app.php';
+
+use Phpmig\Adapter;
+use Pimple;
+use Hall;
+
+$app = new \Hall\App(CONFIG_ROOT.'/default.php');
+$app->registerDatabase();
 
 $container = new Pimple();
-
 $container['db'] = function() {
-    $config = require(CONFIG_ROOT.'/default.php');
-
-    ORM::configure('sqlite:'.$config['database']['host']);
-
-    Model::$auto_prefix_models = '\\Hall\\Model\\';
-
     return ORM::get_db();
 };
-
 $container['phpmig.adapter'] = function() use($container) {
     return new Adapter\PDO\Sql($container['db'],'migrations');
 };
-
 $container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'migrations';
 
 return $container;
